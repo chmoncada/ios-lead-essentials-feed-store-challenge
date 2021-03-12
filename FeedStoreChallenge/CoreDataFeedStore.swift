a7da3a74a98903c9a4c925d4acb93a29e6950ec4
Charles Moncada <chmoncada@me.com>
2021-03-11 20:39:45 -0500

    coreDataFeedStore delivers found values on non empty cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -21,7 +21,26 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		let context = self.context
+		context.perform {
+			do {
+				let request = NSFetchRequest<CoreDataCache>(entityName: CoreDataCache.entity().name!)
+				request.returnsObjectsAsFaults = false
+				if let cache = try context.fetch(request).first {
+					completion(.found(
+								feed: cache.feed
+									.compactMap { $0 as? CoreDataFeedImage }
+									.map {
+										LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+									},
+								timestamp: cache.timestamp))
+				} else {
+					completion(.empty)
+				}
+			} catch {
+				completion(.failure(error))
+			}
+		}
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
@@ -29,6 +48,25 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+		let context = self.context
+		context.perform {
+			do {
+				let cache = CoreDataCache(context: context)
+				cache.timestamp = timestamp
+				cache.feed = NSOrderedSet(array: feed.map{ local in
+					let coreDataFeedImage = CoreDataFeedImage(context: context)
+					coreDataFeedImage.id = local.id
+					coreDataFeedImage.imageDescription = local.description
+					coreDataFeedImage.location = local.location
+					coreDataFeedImage.url = local.url
+					return coreDataFeedImage
+				})
+				try context.save()
+				completion(nil)
+			} catch {
+				completion(error)
+			}
+		}
 
 	}
 }
@@ -57,12 +95,14 @@ private extension NSPersistentContainer {
 	}
 }
 
+@objc(CoreDataCache)
 private class CoreDataCache: NSManagedObject {
 	@NSManaged var timestamp: Date
 	@NSManaged var feed: NSOrderedSet
  }
 
- private class CoreDataFeedImage: NSManagedObject {
+@objc(CoreDataFeedImage)
+private class CoreDataFeedImage: NSManagedObject {
 	@NSManaged var id: UUID
 	@NSManaged var imageDescription: String?
 	@NSManaged var location: String?
